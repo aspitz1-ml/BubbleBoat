2a3c4925e015fd4f7dca3bb4974a6caed6b65480
CobyPear <shagsher@gmail.com>
2025-01-25 14:48:11 -0600

    hook meter up to fuel

--- Assets/Scripts/FuelMeter.cs
+++ Assets/Scripts/FuelMeter.cs
@@ -3,24 +3,21 @@ using UnityEngine.UI;
 
 public class FuelMeter : MonoBehaviour
 {
-    public Image fuelMeterFill;  // Drag the HealthBarFill image here in the Inspector
-    public float maxFuel = 100f;  // Set max health
-    private float currentFuel;
+    public Image fuelMeterFill;
+    public float maxFuel = 100f;
+    public float currentFuel;
 
     void Start()
     {
-        Debug.Log("FuelMeter script started");
-        currentFuel = maxFuel;  // Set initial health
+        currentFuel = maxFuel;  // Set initial fuel
     }
 
     void Update()
     {
-        // For testing, decrease health over time (you can remove this part in the final version)
-        if (Input.GetKey(KeyCode.Space))  // Press space to decrease health
+        if (Fuel.Instance != null)
         {
 
-            Debug.Log("Removing fuel...");
-            currentFuel -= 1f * Time.deltaTime * 10f;  // Decrease health slowly for testing
+            currentFuel = Fuel.Instance.currentFuel;
         }
 
         // Clamp the current health to ensure it doesn't go below 0 or above max
