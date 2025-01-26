c4aed9eee389db8b3b4e0cdeb514be7b4eca5afe
Anna Spitz <anna.spitz@modernlogic.io>
2025-01-26 13:24:33 -0600

    Add more hazards

--- Assets/Scripts/BubbleWand.cs
+++ Assets/Scripts/BubbleWand.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class BubbleWand : MonoBehaviour
 {
 
-    public float fuelAmount = 20;
+    public float fuelAmount = 30;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
