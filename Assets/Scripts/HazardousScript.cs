cb3df63184a2031db84d2c9b86bd8bfd16e89101
Anna Spitz <anna.spitz@modernlogic.io>
2025-01-26 14:03:30 -0600

    Fix hazard on plane

--- Assets/Scripts/HazardousScript.cs
+++ Assets/Scripts/HazardousScript.cs
@@ -10,8 +10,8 @@ public class HazardousScript: MonoBehaviour
             // Reduce the player's health
             if (Fuel.Instance != null)
             {
-                Debug.Log("Hazard hit the boat! Reducing fuel by 10...");
-                Fuel.Instance.AddFuel(-10f);
+                Debug.Log("Hazard hit the boat! Reducing fuel by 20...");
+                Fuel.Instance.AddFuel(-20f);
             }
         }
     }
